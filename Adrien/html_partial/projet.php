4de39bc9af01fda7f877627113345fad2b85cd83
Theo Duval <theoduval@macbook-pro-de-theo.home>
2022-03-16 20:44:19 +0100

    html

--- Adrien/html_partial/projet.php
+++ Adrien/html_partial/projet.php
@@ -1,16 +1,20 @@
+<div id=#title><H1 id="yourProjet">VOS PROJETS</H1></div>
 <section id="section_projet">
     <div class="newProjet">
-    <button class="newProjetBtn"  type="button" onclick="window.location.href = '/new_project';"></btn>
-        <span>Nouveau Projet</span>
-    </button></div>
+        <button class="newProjetBtn" id="" type="button" onclick="window.location.href = '/new_project';"></btn>
+            <span>Nouveau Projet</span>
+        </button>
+        <img id="addProjet" src="img/Icons/plus.png" alt="+">
+    </div>
 
     <?php foreach($projets as $projet): ?>
         <div class="projet">
-            <btn class="projetBtn">
+            <button class="projetBtn" id="">
                 <span id="name"><?= $projet["project_name"]  ?></span>
                 <span id="date"><?=$projet["date"] ?></span>
-                <button class="delete"><img src="img/bin.png" alt="BIN" id="bin"></button>
-            </div>
+            </button>
+            <button class="delete" id=""><img id="dash"src="img/bin.png" alt="BIN" >
+        </div>
             
     
     <?php endforeach; ?>
