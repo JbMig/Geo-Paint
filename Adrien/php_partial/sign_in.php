a1ee524af3a221b3177794e3060d0384a7841e1f
Doud75 <adrienquimbre@gmail.com>
2022-03-17 23:10:12 +0100

    commentaire code php partial

--- Adrien/php_partial/sign_in.php
+++ Adrien/php_partial/sign_in.php
@@ -1,22 +1,33 @@
 <?php
+//mise en tampon pour stockage dans une variable
 ob_start();
+//valeur pour la balisehtml <title>
 $title = "sign in";
+
+// si on est en method POST, on reçoit les donnée du formulaire
+// permet de créer un utilisateur
 if($_SERVER["REQUEST_METHOD"] === "POST") {
-    $mail = filter_input(INPUT_POST, "mail");
+    $mail = filter_input(INPUT_POST, "mail"); //récupère le mail du formulaire
+    //récupère le mdp du formulaire
     $mdp = hash("sha512", filter_input(INPUT_POST, "mdp"));
-    require_once __DIR__ . "/../database/pdo.php";
+    require_once __DIR__ . "/../database/pdo.php"; //je récupère le PDO
+    //requete sql pour trouver dans la database l'utilisateur voulu
     $maRequete = $pdo->prepare("SELECT `mail` FROM `user` WHERE `mail` = :mail;");
     $maRequete->execute([
         ":mail" => $mail
     ]);
 
+    //récupère le résultat de la requète
     $user = $maRequete->fetch();
+    //si l'utilisateur existe déjà dans la database
     if(in_array($mail, $user)) {
         $variant = "danger";
         $message = "l'utilisateur existe déjà";
+        //indique que le serveur refuse d'autoriser la requête 
         http_response_code(403);
+        //j'appelle ma bannière html pour afficher un message d'erreur
         require_once __DIR__ . "/../html_partial/alerts/baniere.php";
-    } else {
+    } else { //sinon j'ajoute le résultat de la requète dans la database
         $maRequete = $pdo->prepare("INSERT INTO `user` (`mail`, `mdp`) VALUES(:mail, :mdp)");
         $maRequete->execute([
             ":mail" => $mail,
@@ -24,11 +35,11 @@ if($_SERVER["REQUEST_METHOD"] === "POST") {
         ]);
 
         http_response_code(302);
-        header('Location: /login');
+        header('Location: /login'); //je vais à la page login
         exit();
     }
 }
-
+//j'appelle l'html de cette page
 require_once __DIR__ . '/../html_partial/sign_in.php';
 
-$content = ob_get_clean();
+$content = ob_get_clean(); //je stock le tampon dans cette variable
