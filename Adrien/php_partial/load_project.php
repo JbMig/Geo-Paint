5790dc180e8161805bfbcccdef06220e7735d080
Doud75 <adrienquimbre@gmail.com>
2022-03-17 13:07:10 +0100

    ça save dur

--- Adrien/php_partial/load_project.php
+++ Adrien/php_partial/load_project.php
@@ -6,9 +6,9 @@ if ("POST" === $_SERVER["REQUEST_METHOD"]) {
     require "../database/pdo.php";
     $user_id = $_SESSION["user"]["id"];
     $data = filter_input(INPUT_POST, "data");
-    $projectName = filter_input(INPUT_POST, "projectName");
+    $projectName = $_SESSION["projet"]["project_name"];
 
-    $maRequete = $pdo->prepare("SELECT `id`, `project_name`, `data` `user_id` FROM `projet` WHERE `project_name` = :projectName AND `user_id` = :userId;");
+    $maRequete = $pdo->prepare("SELECT `id`, `project_name`, `data`, `user_id` FROM `projet` WHERE `project_name` = :projectName AND `user_id` = :userId;");
     $maRequete->execute([
         ":projectName" => $projectName,
         ":userId" => $user_id
@@ -16,7 +16,15 @@ if ("POST" === $_SERVER["REQUEST_METHOD"]) {
 
     $projet_server = $maRequete->fetch();
     
-    if (!isset($_SESSION['project_name'])) {
+    if ($projet_server["project_name"] == $projectName) {
+        $maRequete = $pdo->prepare("UPDATE `projet` SET `data`= :oneData, `date` = CURRENT_TIMESTAMP WHERE `project_name` = :projectName AND `user_id` = :userId;");
+            $maRequete->execute([
+                ":oneData" => $data,
+                ":projectName" => $projectName,
+                ":userId" => $user_id
+            ]);
+    }
+    else {
         $maRequete = $pdo->prepare(
             "INSERT INTO `projet` (`project_name`, `user_id`, `data`)
             VALUES(:projectName, :userId, :oneData)");
@@ -25,29 +33,12 @@ if ("POST" === $_SERVER["REQUEST_METHOD"]) {
                 ":userId" => $user_id,
                 ":oneData" => $data
             ]);
-        $maRequete = $pdo->prepare("SELECT `id`, `project_name`, `data` `user_id` FROM `projet` WHERE `project_name` = :projectName AND `user_id` = :userId;");
-            $maRequete->execute([
-                ":projectName" => $projectName,
-                ":userId" => $user_id
-            ]);
-            $projet_server = $maRequete->fetch();
-            
-    } else {
-        if ($projet_server["project_name"] == $projectName) {
-            $maRequete = $pdo->prepare("UPDATE `projet` SET `data`= :oneData, `date` = CURRENT_TIMESTAMP WHERE `project_name` = :projectName AND `user_id` = :userId;");
-            $maRequete->execute([
-                ":projectName" => $projectName,
-                ":oneData" => $data,
-                ":userId" => $user_id
-            ]);
-        } 
     }
     $_SESSION["projet"] = $projet_server;
     http_response_code(302);
     header('Location: /load_project');
     exit();
 }
-
 require_once __DIR__ . '/../html_partial/load_project.php';
 
 $content = ob_get_clean();
--- Adrien/php_partial/new_project.php
+++ Adrien/php_partial/new_project.php
@@ -9,9 +9,22 @@ if ("POST" === $_SERVER["REQUEST_METHOD"]) {
     $user_id = $_SESSION["user"]["id"];
     $data = filter_input(INPUT_POST, "data");
     $projectName = filter_input(INPUT_POST, "projectName");
+    $maRequete = $pdo->prepare("SELECT `id`, `project_name`, `data`, `user_id` FROM `projet` WHERE `project_name` = :projectName AND `user_id` = :userId;");
+        $maRequete->execute([
+            ":projectName" => $projectName,
+            ":userId" => $user_id
+        ]);
+    $projet_server = $maRequete->fetch();
 
-    
-    if (!isset($_SESSION['project_name'])) {
+    if ($projet_server["project_name"] == $projectName) {
+        $maRequete = $pdo->prepare("UPDATE `projet` SET `data`= :oneData, `date` = CURRENT_TIMESTAMP WHERE `project_name` = :projectName AND `user_id` = :userId;");
+            $maRequete->execute([
+                ":oneData" => $data,
+                ":projectName" => $projectName,
+                ":userId" => $user_id
+            ]);
+    }
+    else {
         $maRequete = $pdo->prepare(
             "INSERT INTO `projet` (`project_name`, `user_id`, `data`)
             VALUES(:projectName, :userId, :oneData)");
@@ -20,31 +33,11 @@ if ("POST" === $_SERVER["REQUEST_METHOD"]) {
                 ":userId" => $user_id,
                 ":oneData" => $data
             ]);
-        $maRequete = $pdo->prepare("SELECT `id`, `project_name`, `data`, `user_id` FROM `projet` WHERE `project_name` = :projectName AND `user_id` = :userId;");
-            $maRequete->execute([
-                ":projectName" => $projectName,
-                ":userId" => $user_id
-            ]);
-        $projet_server = $maRequete->fetch();
-        $_SESSION["projet"] = $projet_server;
-        print_r($_SESSION["projet"]);
-        http_response_code(302);
-        header('Location: /load_project');
-        exit();
-    } else if ($projet_server["project_name"] == $projectName) {
-            $variant = "danger";
-            $message = "nom déjà utilisé";
-            http_response_code(403);
-            require_once __DIR__ . "/../html_partial/alerts/baniere.php";
-            
-            //je ne veux pas écraser un fichier qui porte le même nom (je crois)
-            // $maRequete = $pdo->prepare("UPDATE `projet` SET `data`= :oneData, `date` = CURRENT_TIMESTAMP WHERE `project_name` = :projectName AND `user_id` = :userId;");
-            // $maRequete->execute([
-            //     ":oneData" => $data,
-            //     ":projectName" => $projectName,
-            //     ":userId" => $user_id
-            // ]);
     }
+    $_SESSION["projet"] = $projet_server;
+    http_response_code(302);
+    header('Location: /load_project');
+    exit();
     
 }
 
