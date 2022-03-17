1e39c1c643e8b0036c12f0d931f20c7156b391c0
Doud75 <adrienquimbre@gmail.com>
2022-03-17 12:39:18 +0100

    impossible de nommer projet qui existe deja

--- Adrien/php_partial/new_project.php
+++ Adrien/php_partial/new_project.php
@@ -20,27 +20,32 @@ if ("POST" === $_SERVER["REQUEST_METHOD"]) {
                 ":userId" => $user_id,
                 ":oneData" => $data
             ]);
-    } else {
-        if ($projet_server["project_name"] == $projectName) {
-            $maRequete = $pdo->prepare("UPDATE `projet` SET `data`= :oneData, `date` = CURRENT_TIMESTAMP WHERE `project_name` = :projectName AND `user_id` = :userId;");
+        $maRequete = $pdo->prepare("SELECT `id`, `project_name`, `data`, `user_id` FROM `projet` WHERE `project_name` = :projectName AND `user_id` = :userId;");
             $maRequete->execute([
-                ":oneData" => $data,
                 ":projectName" => $projectName,
                 ":userId" => $user_id
             ]);
-        } 
+        $projet_server = $maRequete->fetch();
+        $_SESSION["projet"] = $projet_server;
+        print_r($_SESSION["projet"]);
+        http_response_code(302);
+        header('Location: /load_project');
+        exit();
+    } else if ($projet_server["project_name"] == $projectName) {
+            $variant = "danger";
+            $message = "nom déjà utilisé";
+            http_response_code(403);
+            require_once __DIR__ . "/../html_partial/alerts/baniere.php";
+            
+            //je ne veux pas écraser un fichier qui porte le même nom (je crois)
+            // $maRequete = $pdo->prepare("UPDATE `projet` SET `data`= :oneData, `date` = CURRENT_TIMESTAMP WHERE `project_name` = :projectName AND `user_id` = :userId;");
+            // $maRequete->execute([
+            //     ":oneData" => $data,
+            //     ":projectName" => $projectName,
+            //     ":userId" => $user_id
+            // ]);
     }
-    $maRequete = $pdo->prepare("SELECT `id`, `project_name`, `data`, `user_id` FROM `projet` WHERE `project_name` = :projectName AND `user_id` = :userId;");
-            $maRequete->execute([
-                ":projectName" => $projectName,
-                ":userId" => $user_id
-            ]);
-            $projet_server = $maRequete->fetch();
-    $_SESSION["projet"] = $projet_server;
-    print_r($_SESSION["projet"]);
-    http_response_code(302);
-    header('Location: /load_project');
-    exit();
+    
 }
 
 require_once __DIR__ . '/../html_partial/new_project.php';
