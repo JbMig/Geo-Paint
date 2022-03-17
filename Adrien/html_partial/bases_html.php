d83773637fce3ea710276c053a1a1722526d20a9
Doud75 <adrienquimbre@gmail.com>
2022-03-17 16:10:15 +0100

    titre pour png js

--- Adrien/html_partial/bases_html.php
+++ Adrien/html_partial/bases_html.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?=$title?></title>
+    <title id="titre"><?=$title?></title>
     <link rel="stylesheet" type="text/css" href="style/homePaints.css?<?php echo time();?>">
 </head>
 <body>
@@ -12,7 +12,6 @@
     <?= $content ?>
 
     <footer>
-        <!-- <script src="script/script.js?<?php echo time(); ?>"></script> -->
     </footer>
 </body>
 </html>
