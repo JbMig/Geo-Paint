f2ae4cefe3d1385df56e82f7d3f0ff0aea39402c
Theo Duval <theoduval@macbook-pro-de-theo.home>
2022-03-17 22:05:54 +0100

    login+sign

--- Adrien/html_partial/login.php
+++ Adrien/html_partial/login.php
@@ -7,7 +7,7 @@
             <h1 class="welcome">Welcome To <p id="something">Draw Something</p></h1>
             <div class="formInput">
                 <form class="form" action="/login" method="post">
-                    <div><p Class="Emails">E-mail</p><input type="email" id="email" name="mail" placeholder=""></div>
+                    <div><p Class="emails">E-mail</p><input type="email" id="email" name="mail" placeholder=""></div>
                     <div><p Class="passwords">Password</p><input type="password" id="password" name="mdp" placeholder=""></div>
                     <div><button type="submit" id="valider" name="valider" >Valider</button>
                 </form>
--- Adrien/html_partial/sign_in.php
+++ Adrien/html_partial/sign_in.php
@@ -1,14 +1,14 @@
-<section class="bodyLogin">
+<body class="bodyLogin">
     <main class="loginMenu">
         <div class="block1">
             <h1 class="welcome">Sign up to <p id="something">Draw Something</p></h1>
             <div class="formInput">
                 <form method="POST">
-                    <div><p Class="emailsign">Entrer votre E-mail</p><input type="email" id="email" name="mail" placeholder=""></div>
-                    <div><p Class="passwordssign">Entrer votre Password</p><input type="password" id="password" name="mdp" placeholder=""></div>
+                    <div><p Class="emailsign">Entrer votre <span class="green">E-mail :</span></p><input type="email" id="email" name="mail" placeholder=""></div>
+                    <div><p Class="passwordssign">Entrer votre <span class="green">Password :</span></p><input type="password" id="password" name="mdp" placeholder=""></div>
                     <div><input type="submit" id="valider" value="Valider"/></div>
                 </form>
             </div>
         </div>
     </main>
-</section>
+</body>
