bf593bfc981663f859e454f1748293e5fa1de33f
Doud75 <adrienquimbre@gmail.com>
2022-03-18 11:06:19 +0100

    commentaire code router

--- Adrien/router.php
+++ Adrien/router.php
@@ -25,26 +25,32 @@ if ($uri == '/entrer.php') {
 }
 
 
-
+//si $uri
 switch ($uri) {
-    case '/login':
-        require_once __DIR__ . '/php_partial/login.php';
+    case '/login': // est égale à login
+        //depuis la racine va chercher login.php
+        require_once __DIR__ . '/php_partial/login.php'; 
         break;
-    case '/sign_in':
+    case '/sign_in':// est égale à sign_in
+        //depuis la racine va chercher sign_in.php
         require_once __DIR__ . '/php_partial/sign_in.php';
         break;
-    case '/projet':
+    case '/projet':// est égale à projet
+        //depuis la racine va chercher projet.php
         require_once __DIR__ . '/php_partial/projet.php';
         break;
-    case "/new_project":
+    case "/new_project":// est égale à new_project
+        //depuis la racine va chercher New_project
         require_once __DIR__ . '/php_partial/new_project.php';
         break;
-    case "/load_project":
+    case "/load_project":// est égale à load_project
+        //depuis la racine va chercher loed_project.php
         require_once __DIR__ . '/php_partial/load_project.php';
         break;
-    default:
-        http_response_code(404);
+    default: //sinon
+        http_response_code(404); //erreur 404
         $content = file_get_contents(__DIR__ . '/html_partial/alerts/404.php');
 }
+//j'appelle la base html qui affichera $content (voir dans les fichiers dans php_partial)
 require_once __DIR__ . '/html_partial/bases_html.php';
 ?>
