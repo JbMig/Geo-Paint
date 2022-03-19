4166e8389ccb1b8e37fcc7f62f63f6ff1996abe9
Doud75 <adrienquimbre@gmail.com>
2022-03-19 15:12:48 +0100

    changement projet en formulaire pour supprimer (plus propre)

--- Adrien/php_partial/projet.php
+++ Adrien/php_partial/projet.php
@@ -18,21 +18,34 @@ $projets = $maRequete->fetchAll(PDO::FETCH_ASSOC);
 // si on est en method POST, on reçoit les donnée du formulaire
 // permet de charger un projet en récupèrent ses données dans la data base
 if ("POST" === $_SERVER["REQUEST_METHOD"]) {
-    //récupère le nom du projet
-    $projet = filter_input(INPUT_POST, "project_name");
-    //requete sql pour trouver dans la database le projet voulu
-    //on utilise le nom du projet et l'id utilisateur
-    $maRequete = $pdo->prepare("SELECT `id`, `project_name`, `data`, `user_id` FROM `projet` WHERE `project_name` = :projectName AND `user_id` = :userId;");
-    $maRequete->execute([
-        ":projectName" => $projet,
-        ":userId" => $user_id
-    ]);
-    //récupère le résultat de la requète
-    $projet_server = $maRequete->fetch();
-    //j'ajoute le projet à la session
-    $_SESSION["projet"] = $projet_server;
-    header("Location: /load_project"); //je vais à la page projet chargé
-    exit();
+    if(isset($_POST["project_name"])) {
+        //récupère le nom du projet
+        $projet = filter_input(INPUT_POST, "project_name");
+        //requete sql pour trouver dans la database le projet voulu
+        //on utilise le nom du projet et l'id utilisateur
+        $maRequete = $pdo->prepare("SELECT `id`, `project_name`, `data`, `user_id` FROM `projet` WHERE `project_name` = :projectName AND `user_id` = :userId;");
+        $maRequete->execute([
+            ":projectName" => $projet,
+            ":userId" => $user_id
+        ]);
+        //récupère le résultat de la requète
+        $projet_server = $maRequete->fetch();
+        //j'ajoute le projet à la session
+        $_SESSION["projet"] = $projet_server;
+        header("Location: /load_project"); //je vais à la page projet chargé
+        exit();
+    } else if(isset($_POST["delete_id"])) {
+        $id = filter_input(INPUT_POST, "delete_id");
+        if($id) {
+            $maRequete = $pdo->prepare("DELETE FROM `projet` WHERE `id` = :id");
+            $maRequete->execute([
+                ":id" => $id
+            ]);
+            http_response_code(302);
+            header('Location: /projet');
+            exit();
+        }
+    }
 }
 
 //j'appelle l'html de cette page
